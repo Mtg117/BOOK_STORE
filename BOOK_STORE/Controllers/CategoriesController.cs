2370d6e91236bb2ee86ab2ce77befc9beac1d784
EasyLife <EasyLife@M-T-G>
2024-05-29 18:07:39 +0300

    show categories

---
+++ BOOK_STORE/Controllers/CategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using BOOK_STORE.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BOOK_STORE.Controllers
+{
+	public class CategoriesController : Controller
+	{
+		private readonly ApplicationDbContext context;
+
+		public CategoriesController(ApplicationDbContext context)
+		{
+			this.context = context;
+		}
+		public IActionResult Index()
+		{
+			var categories = context.Categories.ToList();
+			return View(categories);
+		}
+	}
+}
--- BOOK_STORE/Data/ApplicationDbContext.cs
+++ BOOK_STORE/Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using BOOK_STORE.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BOOK_STORE.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Category> Categories { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
